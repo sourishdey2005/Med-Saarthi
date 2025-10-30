547da1e24042be4ca5232e928785be97d1ddb1ab
sourish dey <sourish713321@gmail.com>
2025-10-30 17:32:05 +0000

    Error: ENOENT: no such file or directory, lstat '/vercel/path0/.next/ser

--- src/app/(app)/page.tsx
+++ src/app/(app)/page.tsx
@@ -1,11 +1,5 @@
-'use client'
-
 import { redirect } from 'next/navigation'
-import { useEffect } from 'react'
 
 export default function AppRootPage() {
-  useEffect(() => {
-    redirect('/dashboard')
-  }, [])
-  return null
+  redirect('/dashboard')
 }
